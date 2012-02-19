1fc83a78a2d7f0e015f2d0d742f0767bc3b9a2f4
AlphaHydrae <hydrae.alpha@gmail.com>
2012-02-19 13:34:38 +0100

    Added specs for validations, emitting with arguments, and chaining.

--- spec/basics.js
+++ spec/basics.js
@@ -4,18 +4,39 @@ var vows = require('vows');
 
 var EventEmitter = require('../src/eventful.js').EventEmitter;
 
-vows.describe('EventEmitter').addBatch({
+vows.describe('Basics').addBatch({
 
   'An event emitter' : {
     topic : function() {
       return new EventEmitter();
     },
 
-    'should respond to on, off, emit, withNamespace' : function(topic) {
+    'should respond to on, off, emit, withNamespace, withoutNamespace' : function(topic) {
       assert.isFunction(topic.on);
       assert.isFunction(topic.off);
       assert.isFunction(topic.emit);
       assert.isFunction(topic.withNamespace);
+      assert.isFunction(topic.withoutNamespace);
+    },
+
+    'when called' : {
+      topic : function(ee) {
+        var chainOne = ee.on('foo', function() {});
+        var chainTwo = chainOne.off('foo');
+        var chainThree = chainTwo.emit('foo');
+        var chainFour = chainThree.withoutNamespace();
+        // withNamespace is not tested as it returns a wrapper
+        return {
+          original: ee,
+          chain: [ chainOne, chainTwo, chainThree, chainFour ]
+        };
+      },
+
+      'should return itself' : function(topic) {
+        for (var i in topic.chain) {
+          assert.strictEqual(topic.original, topic.chain[i]);
+        }
+      }
     }
   },
 
@@ -39,6 +60,18 @@ vows.describe('EventEmitter').addBatch({
 
     'should not call it back when emitting foo' : function(topic) {
       assert.isFalse(topic.barCalled);
+    }
+  },
+
+  'An event emitter which emits foo with arguments' : {
+    topic : function() {
+      new EventEmitter().on('foo', this.callback).emit('foo', 'b', 'a', 'r');
+    },
+
+    'should pass those arguments to foo callbacks' : function(arg1, arg2, arg3) {
+      assert.equal(arg1, 'b');
+      assert.equal(arg2, 'a');
+      assert.equal(arg3, 'r');
     }
   }
 
